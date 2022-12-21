aeedfb9126d1f547bfc5e636a87f200982fa0681
Riky Dermawan <114652908+rikyd17@users.noreply.github.com>
2022-12-21 12:06:40 +0700

    update

---
+++ Frontend/collagen/lib/Screens/NavBarBody/MenuBody.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MoreBody extends StatefulWidget {
+  const MoreBody({super.key});
+
+  @override
+  _MoreBody createState() => _MoreBody();
+}
+
+class _MoreBody extends State<MoreBody> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.square(60),
+          child: Container(
+            decoration: const BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 2.0),
+                blurRadius: 4.0,
+              )
+            ]),
+            child: AppBar(
+              backgroundColor: Colors.white,
+              title: const Text(
+                  "Menu"
+              ),
+              titleTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
