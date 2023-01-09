ec73258a1c085b5ea00e42699a03f10a174fc5d7
agnimayaa <94313497+agnimayaa@users.noreply.github.com>
2023-01-09 21:21:08 +0700

    Update session.dart

--- Frontend/collagen/lib/utils/session.dart
+++ Frontend/collagen/lib/utils/session.dart
@@ -1,4 +1,4 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'show SharedPreferences;
 
 class Session {
   static final String ACCOUNT_SESSION_KEY = 'accountModel';
