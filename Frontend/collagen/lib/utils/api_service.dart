1a6610906677735feea10e7a9b4be8bb86756a3f
agnimayaa <94313497+agnimayaa@users.noreply.github.com>
2023-01-09 18:54:35 +0700

    Update api_service.dart

---
+++ Frontend/collagen/lib/utils/api_service.dart
@@ -0,0 +1,12 @@
+class api_service {
+  //static const String SOCMED_END_POINT = 'https://driveid.prune.id/auth/api';
+  //static const String SOCMED_END_POINT = 'http://172.24.1.165:3000/auth/api';
+  //static const String IMAGE_URL = 'http://172.24.1.165:3000';
+  static const String SOCMED_END_POINT = 'http://[::1]:7090/api/';
+
+  static const String DATE_TIME_FORMAT = 'MMM dd, yyyy HH:mm';
+  static const String DATE_FORMAT = 'MMMM d, y';
+  static const String TIME_FORMAT = 'HH:mm';
+  static const String DATE_SERVER_FORMAT = 'yyyy-MM-d';
+  static const String DATE_TIME_SERVER_FORMAT = 'yyyy-MM-d HH:mm';
+}
